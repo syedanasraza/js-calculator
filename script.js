ea25519b0465816bc202164c2ddc56841744f045
syedanasraza <ar3942635@gmail.com>
2024-12-07 20:11:31 -0800

    calculator

---
+++ script.js
@@ -0,0 +1,29 @@
+function add(){
+}
+function subtract(){
+    let num1 = prompt("enter your first number")
+    let num2 = prompt("enter your second number")
+
+    num1 = Number(num1)
+    num2 = Number(num2)
+
+    alert(num1-num2)
+}
+function multiply(){
+    let num1 = prompt("enter your first number")
+    let num2 = prompt("enter your second number")
+
+    num1 = Number(num1)
+    num2 = Number(num2)
+
+    alert(num1*num2)
+}
+function divide(){
+    let num1 = prompt("enter your first number")
+    let num2 = prompt("enter your second number")
+
+    num1 = Number(num1)
+    num2 = Number(num2)
+
+    alert(num1/num2)
+}
